61450f28c676d3aea45b7c0af9fbbfa5b5709619
Luchezar_a <lachezaratanasov1705@gmail.com>
2023-07-03 01:16:36 +0300

    Db models and db context added.

---
+++ TicketingSystem/TicketingSystem.Entities/Data/TicketingSystemDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystem.Entities.Models;
+
+namespace TicketingSystem.Entities.Data
+{
+    public class TicketingSystemDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public TicketingSystemDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected TicketingSystemDbContext()
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ TicketingSystem/TicketingSystem.Entities/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketingSystem.Entities.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        [Required]
+        [StringLength(30)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Lastname { get; set; }
+    }
+}
